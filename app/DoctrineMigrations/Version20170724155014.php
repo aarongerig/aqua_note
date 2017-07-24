b832bf7365507a73f03e5ebb30070d5ac030238a
Aaron Gerig <geoisborn@gmail.com>
2017-07-24 18:59:15 +0200

    Doctrine migrations

---
+++ app/DoctrineMigrations/Version20170724155014.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Platforms\MySqlPlatform;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170724155014 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function preUp(Schema $schema)
+    {
+        parent::preUp($schema);
+        $this->setForeignKeyChecks(false);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function postUp(Schema $schema)
+    {
+        parent::postUp($schema);
+        $this->setForeignKeyChecks(true);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function preDown(Schema $schema)
+    {
+        parent::preDown($schema);
+        $this->setForeignKeyChecks(false);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function postDown(Schema $schema)
+    {
+        parent::postDown($schema);
+        $this->setForeignKeyChecks(true);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE genus_note DROP FOREIGN KEY FK_6478FCEC85C4074C');
+        $this->addSql('ALTER TABLE genus_note CHANGE genus_id genus_id INT NOT NULL');
+        $this->addSql('ALTER TABLE genus_note ADD CONSTRAINT FK_6478FCEC85C4074C FOREIGN KEY (genus_id) REFERENCES genus (id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE genus_note CHANGE genus_id genus_id INT DEFAULT NULL');
+    }
+
+    /**
+     * @param boolean $enabled
+     */
+    protected function setForeignKeyChecks($enabled)
+    {
+        $connection = $this->connection;
+        $platform = $connection->getDatabasePlatform();
+
+        if ($platform instanceof MySqlPlatform) {
+            $connection->exec(sprintf('SET foreign_key_checks = %s;', (int)$enabled));
+        }
+    }
+}
